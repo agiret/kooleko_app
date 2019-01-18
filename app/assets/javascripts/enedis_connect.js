cac0d93994098cb96a0ea4e872f02269b49868b3
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2019-01-18 16:43:37 +0100

    Test enedis_connect.js

---
+++ app/assets/javascripts/enedis_connect.js
@@ -0,0 +1,7 @@
+const enedis_connect = document.getElementById('enedis-btn')
+if (enedis_connect) {
+  enedis_connect.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log("Hello de enedis_connect !");
+  });
+}
