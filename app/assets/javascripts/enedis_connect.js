a07d9c3e425abb47d8f4d87bb6e3ae5099ccaaae
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2019-01-22 22:20:08 +0100

    Add some comments to prepare dev

--- app/assets/javascripts/enedis_connect.js
+++ app/assets/javascripts/enedis_connect.js
@@ -6,12 +6,14 @@
 
 // Requête :
 const link = 'https://gw.hml.api.enedis.fr/group/espace-particuliers/consentement-linky/oauth2/authorize?client_id=3d5cbbbb-fcf4-4c6a-8c86-f18a5ba156e9&state=fz80ac780&duration=P6M&response_type=code&redirect_uri=https://gw.hml.api.enedis.fr/redirect';
-
+  // essayer de juste lancer l'url > récupérer les params
+  // et revenir avec les données qu'il faut...
 const enedis_connect = document.getElementById('enedis-btn')
 if (enedis_connect) {
   enedis_connect.addEventListener('click', function(event) {
-    event.preventDefault();
+    event.preventDefault(); // pour empécher de recharger la page
     console.log("Hello de enedis_connect !");
+
     fetch(link)
         // .then(response => console.log(response))
         .then(response => response.text())
@@ -22,6 +24,8 @@ if (enedis_connect) {
           console.log(doc.body);
           const script = doc.querySelector("script");
           console.log(script);
+          parseQueryString();
+          console.log(parmas);
         });
   });
-}
+};
