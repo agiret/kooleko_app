2572ef37565c1f4a1154b9aa39ae24a71f6073fd
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2019-01-29 17:12:18 +0100

    Redirect Home to profils#edit when onboarding_step is not complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @onboarding_step = current_user.onboarding_step
+    if @onboarding_step == 1
+      # Bloc liaison ENEDIS
+      redirect_to edit_profil_path(current_user)
+    elsif  @onboarding_step == 2
+      # Formulaire de saisie des infos compélmentaires
+      redirect_to edit_profil_path(current_user)
+    elsif @onboarding_step == 3
+      # Tableau de bord
+    end
   end
 end
