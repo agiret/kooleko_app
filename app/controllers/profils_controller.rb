ce7c4425177f3132d9e62fe767e7c0c7edf32ef7
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2019-01-30 14:59:59 +0100

    Logic sequense on edit_profil flow

--- app/controllers/profils_controller.rb
+++ app/controllers/profils_controller.rb
@@ -1,6 +1,6 @@
 class ProfilsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_profil, only: [:show, :edit, :update]
+  before_action :set_profil, :set_onboarding_step, only: [:show, :edit, :update]
 
   def edit
   end
@@ -15,5 +15,8 @@ class ProfilsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
   def set_profil
     @profil = User.find(params[:id])
+  end
+  def set_onboarding_step
+    @onboarding_step = current_user.onboarding_step
   end
 end
