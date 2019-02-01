b032bb63676508652a553ca73a199a3f820288b5
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2019-02-01 12:54:42 +0100

    Structure of requests but CORS error

--- app/javascript/services/enedis_connect.js
+++ app/javascript/services/enedis_connect.js
@@ -4,10 +4,16 @@ const clientId = document.querySelector("#consent").dataset.cliid;
 // console.log(process.env.ENEDIS_CLIENT_ID);
 const clientSecret = document.querySelector("#consent").dataset.clisecret;
 
+// Utilisation d'un proxy pour contourner probleme :
+  // "No 'Access-Control-Allow-Origin' header is present
+  // on the requested resource—when trying to get data from
+  // a REST API
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
 // En attendant de les récupérer directement par une requête :
-  const usagePointId0 = "12345";
+  const usagePointId0 = "22516914714270";
   const usagePointId = usagePointId0;
-  const code = "12345678";
+  const code = "THd5DVk1q9Sr3c2qWpFlxdHGoP1P89";
 
 // *** FUNCTIONS ***
 
@@ -61,6 +67,48 @@ const getCustomerTokens = (code, clientId, clientSecret) => {
   console.log(`code : ${code}`);
   console.log(`client_id : ${clientId}`);
   console.log(`clientSecret : ${clientSecret}`);
+
+
+
+  fetch(proxyurl + "https://gw.hml.api.enedis.fr/v1/oauth2/token?redirect_uri=https://gw.hml.api.enedis.fr/redirect", {
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      // 'Access-Control-Allow-Origin': '*',
+      // 'Access-Control-Allow-Headers': 'Content-Type',
+      // 'Access-Control-Allow-Credentials': 'true'
+    },
+
+    //make sure to serialize your JSON body
+    body: JSON.stringify({
+      code: code,
+      client_id: clientId,
+      client_secret: clientSecret,
+      // grand_type: authorization_code
+    })
+  })
+  .then( (response) => {
+     //do something awesome that makes the world a better place
+     console.log(response);
+  });
+
+
+//*** ex Ou, si on veut généraliser la requête POST : **
+  // var url = 'https://example.com/profile';
+  // var data = {username: 'example'};
+
+  // fetch(url, {
+  //   method: 'POST', // or 'PUT'
+  //   body: JSON.stringify(data), // data can be `string` or {object}!
+  //   headers:{
+  //     'Content-Type': 'application/json'
+  //   }
+  // }).then(res => res.json())
+  // .then(response => console.log('Success:', JSON.stringify(response)))
+  // .catch(error => console.error('Error:', error));
+//ex ***
+
   //   fetch(link)
   //       // .then(response => console.log(response))
   //       .then(response => response.text())
@@ -81,6 +129,35 @@ const getCustomerTokens = (code, clientId, clientSecret) => {
   //       // });
 }
 
+
+const refreshTokens = (refreshToken, clientId, clientSecret) => {
+  console.log("4 - Renouvellement jetons client (refresh_token, client_id_, client_secret) --> acces_token, refresh_token");
+  console.log("... Enregistrer access_token + refresh_token dans DB.users");
+
+  fetch("https://gw.hml.api.enedis.fr/v1/oauth2/token?redirect_uri=https://gw.hml.api.enedis.fr/redirect", {
+    method: "POST",
+    // dataType: 'jsonp',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      // 'Access-Control-Allow-Origin': '*',
+      // 'Access-Control-Allow-Headers': 'Content-Type',
+      // 'Access-Control-Allow-Credentials': 'true'
+    },
+
+    //make sure to serialize your JSON body
+    body: JSON.stringify({
+      refresh_token: refreshToken,
+      client_id: clientId,
+      client_secret: clientSecret,
+      // grand_type: refresh_token
+    })
+  })
+  .then( (response) => {
+     //do something awesome that makes the world a better place
+     console.log(response);
+  });
+}
 // *** END FUNCTIONS ***
 
 
@@ -92,11 +169,15 @@ const enedisLink = () => {
     enedis_connect.addEventListener('click', function(event) {
       event.preventDefault(); // pour empécher de recharger la page
       const state = `${userId - 1}` // en attendant mieux... et pour Clients test ENEDIS
-      consentCall();
-      addHousingToUser(usagePointId);
-      getCustomerTokens(code, clientId, clientSecret);
+      // consentCall();
+      // addHousingToUser(usagePointId);
+      // getCustomerTokens(code, clientId, clientSecret);
+
+      //Test :
+      const refreshToken = "fUTh2rDVQimqjpwbuIcx84v8OGEO6l1nrUMBHbJpv5RxcR";
+      refreshTokens(refreshToken, clientId, clientSecret);
     });
   };
-}
+};
 
 export { enedisLink };
