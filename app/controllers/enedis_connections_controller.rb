5f0b1f06e151d7a251f79a21d30d1508df039b34
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2019-02-08 23:43:08 +0100

    Function refresh_tokens ok

--- app/controllers/enedis_connections_controller.rb
+++ app/controllers/enedis_connections_controller.rb
@@ -1,18 +1,26 @@
 class EnedisConnectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_profil, only: [:connect]
+  before_action :set_profil, :set_keys, only: [:connect]
 
   def connect
     # consent
     # (pour le moment pas de consent par rappor au redirect_uri)
-    get_tokens
+    # get_tokens
+    refresh_tokens
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  # Use callbacks to share common setup or constraints between actions.
+  # BEFORE_ACTION FUNCTIONS:
   def set_profil
     @profil = User.find(current_user.id)
   end
+  def set_keys
+    @client_id = ENV['ENEDIS_CLIENT_ID']
+    @client_secret = ENV['ENEDIS_CLIENT_SECRET']
+  end
+
+  # ENEDIS ACCESS FUNCTIONS :
   def consent
     # affecter une valeur à enedis_state du current_user
     if @profil.enedis_state == nil
@@ -20,7 +28,6 @@ class EnedisConnectionsController < ApplicationController
       @profil.save
     end
     @state = @profil.enedis_state
-    client_id = ENV['ENEDIS_CLIENT_ID']
     link = 'https://gw.hml.api.enedis.fr/group/espace-particuliers/consentement-linky/oauth2/authorize'
     # response = RestClient.get link, {
     #   client_id: client_id,
@@ -32,14 +39,12 @@ class EnedisConnectionsController < ApplicationController
     # @consent_response = response
     duration = 'P6M'
     redirect_uri = 'https://gw.hml.api.enedis.fr/redirect'
-    redirect_to "#{link}?client_id=#{client_id}&state=#{@state}&duration=#{duration}&response_type=code&redirect_uri=#{redirect_uri}"
+    redirect_to "#{link}?client_id=#{@client_id}&state=#{@state}&duration=#{duration}&response_type=code&redirect_uri=#{redirect_uri}"
   end
   def get_tokens
-    client_id = ENV['ENEDIS_CLIENT_ID']
-    client_secret = ENV['ENEDIS_CLIENT_SECRET']
     redirect_uri = 'https://gw.hml.api.enedis.fr/redirect'
     link = "https://gw.hml.api.enedis.fr/v1/oauth2/token"
-    @code = 'qTml8Nb9Ck6o8VOYreXPqb7FrunC2U'  # normalement à récupérer avec consent juste avant
+    @code = 'fz8Ij9bED6fgF1Xk4G5tqG0AJvtqIM'  #!! normalement à récupérer avec consent juste avant
     # response = RestClient.post link, params, headers
     response = RestClient::Request.execute(
       method: 'POST',
@@ -47,22 +52,33 @@ class EnedisConnectionsController < ApplicationController
       payload: {
         grant_type: 'authorization_code',
         code: @code,
-        client_id: client_id,
-        client_secret: client_secret
+        client_id: @client_id,
+        client_secret: @client_secret
       },
       headers: {
         params: {redirect_uri: redirect_uri }
       }
     )
-
-    @token_response = JSON.parse(response)
-    @refresh_token = @token_response['refresh_token']
-    @access_token = @token_response['access_token']
-
-    @profil.enedis_refresh_token = @refresh_token
-    @profil.enedis_access_token = @access_token
+    token_response = JSON.parse(response)
+    @profil.enedis_refresh_token = token_response['refresh_token']
+    @profil.enedis_access_token = token_response['access_token']
     @profil.save
   end
+  def refresh_tokens
+    link = 'https://gw.hml.api.enedis.fr/v1/oauth2/token?redirect_uri=https://gw.hml.api.enedis.fr/redirect'
+    response = RestClient.post link, {
+      grant_type: 'refresh_token',
+      refresh_token: @profil.enedis_refresh_token,
+      client_id: @client_id,
+      client_secret: @client_secret
+    }
+    refresh_response = JSON.parse(response)
+    @profil.enedis_refresh_token = refresh_response['refresh_token']
+    @profil.enedis_access_token = refresh_response['access_token']
+    @profil.save
+  end
+
+  # ENEDIS DATAS REQUESTS :
   def get_identity
     refresh_tokens
 
@@ -72,19 +88,5 @@ class EnedisConnectionsController < ApplicationController
   end
   def contract_datas
 
-  end
-  def refresh_tokens
-    client_id = ENV['ENEDIS_CLIENT_ID']
-    client_secret = ENV['ENEDIS_CLIENT_SECRET']
-    link = 'https://gw.hml.api.enedis.fr/v1/oauth2/token?redirect_uri=https://gw.hml.api.enedis.fr/redirect'
-    response = RestClient.post link, {
-      grant_type: 'refresh_token',
-      refresh_token: 'ciLHi5vBgX7LxPM6ekewQGMZ0U1sivraUFHVYxObINMpY7',
-      client_id: client_id,
-      client_secret: client_secret
-    }
-    @refresh_response = JSON.parse(response)
-    @refresh_token = @refresh_response['refresh_token']
-    @access_token = @refresh_response['access_token']
   end
 end
