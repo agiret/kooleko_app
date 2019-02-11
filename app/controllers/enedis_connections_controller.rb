a7a7cd332082c0d2fae99386b4c5f5fd130102ca
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2019-02-11 18:34:09 +0100

    Add phone to users table and get_client_infos saved the phone number

--- app/controllers/enedis_connections_controller.rb
+++ app/controllers/enedis_connections_controller.rb
@@ -8,6 +8,7 @@ class EnedisConnectionsController < ApplicationController
     # get_tokens
     # refresh_tokens
     get_identity
+    get_client_infos
   end
 
   private
@@ -41,6 +42,9 @@ class EnedisConnectionsController < ApplicationController
     duration = 'P6M'
     redirect_uri = ENV['ENEDIS_REDIRECT_URI']
     redirect_to "#{link}?client_id=#{@client_id}&state=#{@state}&duration=#{duration}&response_type=code&redirect_uri=#{redirect_uri}"
+
+    # enregistrer 'code' dans une varaible @code
+    # stocker usage_point_id dans housings
   end
   def get_tokens
     redirect_uri = ENV['ENEDIS_REDIRECT_URI']
@@ -84,7 +88,6 @@ class EnedisConnectionsController < ApplicationController
   # ENEDIS DATAS REQUESTS :
   def get_identity
     refresh_tokens
-    @msg = "Récupération de l'identité du client"
     @housing = Housing.find(@profil.housing_id)
     @usage_point_id = @housing.enedis_usage_point_id
 
@@ -98,18 +101,36 @@ class EnedisConnectionsController < ApplicationController
         params: {usage_point_id: "#{@housing.enedis_usage_point_id}" }
       }
     )
-    @identity_response = JSON.parse(response)
-    @title = @identity_response[0]['customer']['identity']['natural_person']['title']
-    @firstname = @identity_response[0]['customer']['identity']['natural_person']['firstname']
-    @lastname = @identity_response[0]['customer']['identity']['natural_person']['lastname']
-    # @profil.enedis_refresh_token = token_response['refresh_token']
-    # @profil.enedis_access_token = token_response['access_token']
-    # @profil.save
+    identity_response = JSON.parse(response)
+    @firstname = identity_response[0]['customer']['identity']['natural_person']['firstname']
+    @lastname = identity_response[0]['customer']['identity']['natural_person']['lastname']
+    @profil.firstname = @firstname
+    @profil.lastname = @lastname
+    @profil.save
 
     puts '---> Identité récupérée'
   end
   def get_client_infos
+    # refresh_tokens
+    @housing = Housing.find(@profil.housing_id)
+    @usage_point_id = @housing.enedis_usage_point_id
 
+    link = "https://gw.hml.api.enedis.fr/v3/customers/contact_data"
+    response = RestClient::Request.execute(
+      method: 'GET',
+      url: link,
+      headers: {
+        accept: 'application/json',
+        authorization: "Bearer #{@profil.enedis_access_token}",
+        params: {usage_point_id: "#{@housing.enedis_usage_point_id}" }
+      }
+    )
+    contact_response = JSON.parse(response)
+    @phone = contact_response[0]['customer']['contact_data']['phone']
+    @profil.phone = @phone
+    @profil.save
+
+    puts '---> Identité récupérée'
   end
   def contract_datas
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_164656) do
+ActiveRecord::Schema.define(version: 2019_02_11_173237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_02_11_164656) do
     t.string "enedis_refresh_token", default: "", null: false
     t.string "firstname"
     t.string "lastname"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["housing_id"], name: "index_users_on_housing_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
