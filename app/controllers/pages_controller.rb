27d1514085137bafe8ed92d54379d47953d38554
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2019-02-14 16:27:53 +0100

    Fix current_user dependance on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,16 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-  before_action :set_profil, only: [:home]
 
   def home
-    @onboarding_step = current_user.onboarding_step
-    if @onboarding_step == 1      # Bloc liaison ENEDIS
-      redirect_to edit_profil_path(current_user)
-    elsif  @onboarding_step == 2  # Formulaire de saisie des infos compélmentaires
-      redirect_to edit_profil_path(current_user)
-    # elsif @onboarding_step == 3   # Tableau de bord
+    if current_user
+      @profil = User.find(current_user.id)
+      @onboarding_step = current_user.onboarding_step
+      if @onboarding_step == 1      # Bloc liaison ENEDIS
+        redirect_to edit_profil_path(current_user)
+      elsif  @onboarding_step == 2  # Formulaire de saisie des infos compélmentaires
+        redirect_to edit_profil_path(current_user)
+      # elsif @onboarding_step == 3   # Tableau de bord
+      end
     end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-  def set_profil
-    @profil = User.find(current_user.id)
   end
 end
