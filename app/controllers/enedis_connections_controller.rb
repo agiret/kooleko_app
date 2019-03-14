1757b22c25b383f0fd9303412a85f2df6e476294
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2019-03-14 19:56:20 +0100

    Add tariff_option on each power record

--- app/controllers/enedis_connections_controller.rb
+++ app/controllers/enedis_connections_controller.rb
@@ -171,7 +171,7 @@ class EnedisConnectionsController < ApplicationController
     # refresh_tokens
     # @housing = Housing.find(@profil.housing_id)
     # @usage_point_id = @housing.enedis_usage_point_id  #!! @profil.housing.enedis_usage_point_id ne fonctionne pas
-    @enedis_datum = EnedisDatum.new(housing_id: @housing.id)  #!! prévoir cas où un enedis_datum basé sur ce @housing existe déjà ??
+    enedis_datum = EnedisDatum.new(housing_id: @housing.id)  #!! prévoir cas où un enedis_datum basé sur ce @housing existe déjà ??
 
     link = "https://gw.hml.api.enedis.fr/v3/customers/usage_points/contracts"
     response = RestClient::Request.execute(
@@ -196,17 +196,17 @@ class EnedisConnectionsController < ApplicationController
     @contract_status = contract_response[0]['customer']['usage_points'][0]['usage_point']['contracts']['contract_status']
     @last_distri_tarif_change_date = contract_response[0]['customer']['usage_points'][0]['usage_point']['contracts']['last_distribution_tariff_change_date']
     # Enregistrement des données :
-    @enedis_datum.usage_point_status = @usage_point_status
-    @enedis_datum.meter_type = @meter_type
-    @enedis_datum.segment = @segment
-    @enedis_datum.subscribed_power = @subscribed_power
-    @enedis_datum.last_activation_date = @last_activation_date
-    @enedis_datum.distri_tarif = @distri_tarif
-    @enedis_datum.offpeak_hours = @offpeak_hours
-    @enedis_datum.contract_type = @contract_type
-    @enedis_datum.contract_status = @contract_status
-    @enedis_datum.last_distri_tarif_change_date = @last_distri_tarif_change_date
-    @enedis_datum.save
+    enedis_datum.usage_point_status = @usage_point_status
+    enedis_datum.meter_type = @meter_type
+    enedis_datum.segment = @segment
+    enedis_datum.subscribed_power = @subscribed_power
+    enedis_datum.last_activation_date = @last_activation_date
+    enedis_datum.distri_tarif = @distri_tarif
+    enedis_datum.offpeak_hours = @offpeak_hours
+    enedis_datum.contract_type = @contract_type
+    enedis_datum.contract_status = @contract_status
+    enedis_datum.last_distri_tarif_change_date = @last_distri_tarif_change_date
+    enedis_datum.save
     puts '---> Données de ligne ENEDIS récupérées'
   end
   def get_address
@@ -243,8 +243,8 @@ class EnedisConnectionsController < ApplicationController
     puts '---> Adresse du logement récupérée'
   end
   def get_courbe_conso(start_date, end_date)
-    puts "Récupération des consos du #{start_date} au #{end_date}..."
-    puts "....."
+    puts "---> Récupération des consos du #{start_date} au #{end_date}..."
+    puts "---> ....."
     refresh_tokens
     @housing = Housing.find(@profil.housing_id)
     @usage_point_id = @housing.enedis_usage_point_id  #!! @profil.housing.enedis_usage_point_id ne fonctionne pas
@@ -271,14 +271,26 @@ class EnedisConnectionsController < ApplicationController
     interval = 1800.seconds
     time = @data_start  # format Datetime
     records = 0
+
+    # Récupération du code de l'option tarifaire du contrat :
+    @enedis_datum = EnedisDatum.where(housing_id: @housing.id)[0]  # format avec .where = [ ]
+    @distri_tarif_contract = @enedis_datum.distri_tarif
+    puts "---> Code option tarifaire du contrat : #{@distri_tarif_contract}"
+    @offpeak_hours_contract = @enedis_datum.offpeak_hours
+    puts "---> Plage horaire heures creuses : #{@offpeak_hours_contract}"
+    @offpeak_start = @offpeak_hours_contract[0..4]  # format = string "22h00"
+    @offpeak_end = @offpeak_hours_contract[-5..-1]  # format = string "06h00"
+
     @data_response.each do |data|
       # tariff_option(data) = ?? (appeler une méthdoe dédiée qui renvoir la réponse à chaque tour)
+      tariff_option = tariff_option(time)
+      puts "---> Conso en #{tariff_option}"
       data = Power.new(
         housing_id: @housing.id,
         power_time: time,
         interval: interval,
         power: data['value'],
-        tariff_option: ""
+        tariff_option: tariff_option
         )
       data.save
       time += interval
@@ -287,10 +299,40 @@ class EnedisConnectionsController < ApplicationController
     puts "---> #{records} enregistrements effectués !"
 
   end
-  def tariff_option(data)
-    # Si distri_tarif == "ST" => return "ST"
-    # Si distri_tarif == "DT" =>
-      # Récupérer offpeak_hours (au format "23h-7h" ; et quand il y'en a 2 ??)
-      # Tester dans quelle plage se situe 'data'
+  def tariff_option(time)
+    if @distri_tarif_contract[-2..-1] == "ST"
+      puts "---> Plage horaire en Simple Tarif"
+      return "ST"
+    elsif @distri_tarif_contract[-2..-1] == "DT"
+      puts "---> Plage horaire en Double Tarif"
+      if time_between?(time, @offpeak_start, @offpeak_end)
+        return "HC"
+      else
+        return "HP"
+      end
+        # prévoir par la suite d'autres formats ? "HP" ou "HC" ou "HP" ou autres ?
+    else
+      puts "---> Code distri_tarif '#{@distri_tarif}' non pris en charge !"
+    end
   end
+  def time_between?(datetime, offpeak_start, offpeak_end)
+    # Convertir datetime en un string au format "13h41" :
+    time = datetime.strftime("%Hh%M")
+    puts "---> Power time : #{time}"
+    # Conversion des heures en minutes :
+    time = time[0..1].to_i * 60 + time[-2..-1].to_i
+    offpeak_start = offpeak_start[0..1].to_i * 60 + offpeak_start[-2..-1].to_i
+    offpeak_end = offpeak_end[0..1].to_i * 60 + offpeak_end[-2..-1].to_i
+    # Test si offpeak_start est avant offpeak_end
+    if (offpeak_start < offpeak_end)
+      # renvoi true si time est entre [offpeak_start ; offpeak_end]
+      return offpeak_start <= time && time <= offpeak_end;
+    # Sinon, offpeak_start est après offpeak_end, donc on fait la comparaison opposée
+    else
+      # renvoi !true (=false) si time est entre [offpeak_end ; offpeak_start]
+      # renvoi !false (=true) si time n'est pas entre [offpeak_end ; offpeak_start]
+      return !(offpeak_end < time && time < offpeak_start)
+    end
+  end
+
 end
